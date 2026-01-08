540198f2e91dbb049918cbdfd4210d8b4a4d9c49
noelle-mccall <noelle.mccall@ahead.com>
2026-01-08 19:07:21 +0000

    Search value parsed

--- streamline_app.py
+++ streamline_app.py
@@ -20,4 +20,27 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+
+ingredient_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+ingredients_string = ''
+if(ingredient_list):
+    for fruit_chosen in ingredient_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrient Information')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+
+time_to_insert = st.button('Submit Order')
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
